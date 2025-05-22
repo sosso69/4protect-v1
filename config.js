a5c2648a22452d56e51a98ccf934fb950e9bccae
sosso69 <139251107+sosso69@users.noreply.github.com>
2025-05-22 19:41:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        prefixe: '.', // Préfixe - Le texte avant les commandes
+        buyer: '1053594840442621982', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'Sosso', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
